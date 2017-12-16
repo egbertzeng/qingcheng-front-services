a493d2634936517e72e20edd89fa96b68f54b66e
liguohua <liguohua@cloud-star.com.cn>
2017-12-16 22:28:03 +0800

    init dev branch by liguohua

---
+++ qingcheng_vue2/src/components/user/Index/Footer/about/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ui fluid container">
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+    <div class="ui fluid container">about</div>
+  </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        data () {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+</style>
